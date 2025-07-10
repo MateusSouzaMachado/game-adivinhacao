44188f75fab0e7c2279c5a54ccd5dbc5b2eb9f41
Mateus Souza Machado <mateus2005machado@gmail.com>
2025-07-09 21:45:45 -0300

    classi,css,js

---
+++ resources/views/classic/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ATSUM</title>
+    @vite("resources/css/classic/index.css")
+    @vite("resources/js/classic/index.js")
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Exo:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    
+</head>
+<body style="background-color: #303F9F">
+    <div class="container">
+        <div class="row">
+            <h1 class="game-name">ATSUM</h1>
+            <h2 class="game-difficulty">{{$dificuldade}}</h2>
+            <img src="{{'https://blog.infnet.com.br/wp-content/uploads/sites/6/2023/06/ruby-programming-language.png'}}" alt="Imagem do Jogo">
+        </div>
+    </div>
+</body>
+</html>
